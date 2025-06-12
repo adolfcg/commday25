2a1217bbdf28e4aa5aa0d6e89af10a4e15da2061
Adolfo <adolph.cg@gmail.com>
2025-06-11 18:51:17 -0600

    Workaround to fix the assets stage

--- demo/lib/pipeline-stack.ts
+++ demo/lib/pipeline-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, ShellStep, CodePipelineSource } from 'aws-cdk-lib/pipelines';
 import { SecretValue } from 'aws-cdk-lib';
-import { LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
+import { LinuxBuildImage, BuildSpec } from 'aws-cdk-lib/aws-codebuild';
 import { DemoStage } from './demo-stage';
 
 export class PipelineStack extends cdk.Stack {
@@ -23,11 +23,28 @@ export class PipelineStack extends cdk.Stack {
         ],
         primaryOutputDirectory: 'demo/cdk.out',  
       }),
-      // This will ensure Node.js 18/20 compatibility for all CodeBuild projects
+      // This will ensure Node.js 18/20 compatibility and fix npm registry issues
       codeBuildDefaults: {
         buildEnvironment: {
           buildImage: LinuxBuildImage.STANDARD_7_0,
         },
+      },
+      // Apply the npm registry fix to asset publishing projects
+      assetPublishingCodeBuildDefaults: {
+        buildEnvironment: {
+          buildImage: LinuxBuildImage.STANDARD_7_0,
+        },
+        partialBuildSpec: cdk.aws_codebuild.BuildSpec.fromObject({
+          phases: {
+            install: {
+              commands: [
+                'npm config set registry https://registry.npmmirror.com/ --global',
+                'npm cache clean --force',
+                'npm install -g cdk-assets@latest'
+              ]
+            }
+          }
+        }),
       },
     });
 
